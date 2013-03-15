96b10922374891ce21a2071830176992d3108fcc
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-03-15 17:14:43 +0100

    [BUGFIX] Better configure HTML tables

--- htdocs/typo3conf/ext/speciality/Configuration/TypoScript/Plugin/tx_cssstyledcontent.ts
+++ htdocs/typo3conf/ext/speciality/Configuration/TypoScript/Plugin/tx_cssstyledcontent.ts
@@ -4,3 +4,14 @@
 
 # Remove default CSS of css_styled_content
 plugin.tx_cssstyledcontent._CSS_DEFAULT_STYLE >
+
+# Configure table
+lib.parseFunc_RTE {
+	externalBlocks {
+		table.stdWrap.HTMLparser.tags.table.fixAttrib.class {
+			default = table table-striped table-hover table-condensed
+			always = 1
+			list >
+		}
+	}
+}
