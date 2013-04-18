015c8cb2fdcae81783795dff15f6126c8108a54c
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-04-18 18:46:32 +0200

    [TASK] Anticipate bug with TYPO3 6.1

--- htdocs/typo3conf/ext/speciality/Configuration/TCA/BeUsers.php
+++ htdocs/typo3conf/ext/speciality/Configuration/TCA/BeUsers.php
@@ -2,6 +2,10 @@
 
 // Configure BeUsers
 
+# @todo adjust compatibility TYPO3 6.1
+# * TCA files should now be located at ext:extensionname/Configuration/TCA/tablename.php' and should return
+# an array with the TCA for the table specified by the filename.
+
 # Allows BE User to be put on different page as pid = 0
 \TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('be_users');
 $TCA['be_users']['ctrl']['rootLevel'] = -1;
--- htdocs/typo3conf/ext/speciality/ext_tables.php
+++ htdocs/typo3conf/ext/speciality/ext_tables.php
@@ -16,7 +16,8 @@ $userTsConfig = \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl(
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig($userTsConfig);
 
 # Include custom TCA
-$recordTypes = array('Pages', 'BeUsers');
+# @todo adjust compatibility TYPO3 6.1
+$recordTypes = array('BeUsers');
 $configurationPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . '/Configuration/TCA';
 foreach ($recordTypes as $recordType) {
 	$configurationFile = $configurationPath . '/' . $recordType . '.php';
@@ -25,3 +26,25 @@ foreach ($recordTypes as $recordType) {
 	}
 }
 
+// Configure Pages
+# @todo adjust compatibility TYPO3 6.1
+\TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('pages');
+
+// New icons for the BE
+if (TYPO3_MODE == 'BE') {
+
+	$icons = array('category', 'comment', 'storage', 'tesseract', 'news', 'people');
+	foreach ($icons as $icon) {
+
+		\TYPO3\CMS\Backend\Sprite\SpriteManager::addTcaTypeIcon(
+			'pages',
+			'contains-' . $icon,
+			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Backend/Icons/' . $icon . '.png');
+
+		$TCA['pages']['columns']['module']['config']['items'][] = array(
+			ucfirst($icon),
+			$icon,
+			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Backend/Icons/' . $icon . '.png'
+		);
+	}
+}
