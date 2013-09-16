e7fd2f20a366a1d7fdd8ef978209e160ce49710e
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-09-16 22:45:59 +0200

    [TASK] Remove language menu in BE

--- htdocs/typo3conf/AdditionalConfiguration.php
+++ htdocs/typo3conf/AdditionalConfiguration.php
@@ -15,9 +15,6 @@ if (file_exists($databaseCredentialsFile)) {
 # Frontend Settings
 #$GLOBALS['TYPO3_CONF_VARS']['FE']['pageNotFound_handling'] = '404.html';
 
-# Menu for clearing the language cache
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['lang']['cache']['clear_menu'] = TRUE;
-
 // Add some more prefix table for the live search (the top right search)
 // Usage for editor: #user:foo
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['livesearch']['fe_user'] = 'fe_users';
