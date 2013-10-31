3dab82131440beb29788342f194272f9aa159e7c
Fabien Udriot <fabien.udriot@ecodev.ch>
2013-11-01 00:22:30 +0100

    [CLEANUP] Remove settings file

--- configuration/Settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = 'bootstrapfab';
-$GLOBALS['TYPO3_CONF_VARS']['DB']['host'] = '127.0.0.1';
-$GLOBALS['TYPO3_CONF_VARS']['DB']['password'] = 'root';
-$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = 'root';
-?>
