5c4f4eb042e4f227b6307ca4ed70aa6c94ae7cd7
Ian Excedo <ian@webian.it>
2014-02-03 12:19:35 +0100

    [FEATURE] Added typoscript parameter speciality.debug_css
    speciality.debug_css: if set to 1 the CSS files loaded by EXT:speciality/Resources/Private/Partials/SharedResources.html won't be merged and won't be the minified version.
    This is useful while debug/inspect CSS of the website.
    
    Fixes: #71

---
+++ htdocs/typo3conf/ext/speciality/Configuration/TypoScript/Config/speciality.ts
@@ -0,0 +1,10 @@
+###################################################
+# Speciality
+###################################################
+
+speciality {
+	# Need to debug/inspect CSS files? Then set this to 1 so the CSS files loaded
+	# by EXT:speciality/Resources/Private/Partials/SharedResources.html won't be merged
+	# and won't be the minified version.
+	debug_css = 0
+}
